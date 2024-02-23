bc19d7b91bf2d341c973711d46307e05325f0dfe
Dax Harris <dharr@lle.rochester.edu>
2024-02-23 14:46:57 -0500

    add output element types

--- mantine/lib/components/types.ts
+++ mantine/lib/components/types.ts
@@ -8,7 +8,27 @@ import { StyleProp } from "@mantine/core";
 import * as TablerIcons from "@tabler/icons-react";
 import React from "react";
 
-export type ElementTypes<TE, TS, TP> = StackElement<TE, TS, TP>;
+export type ElementTypes<TE, TS, TP> =
+    | StackElement<TE, TS, TP>
+    | GroupElement<TE, TS, TP>
+    | FlexElement<TE, TS, TP>
+    | SimpleGridElement<TE, TS, TP>
+    | BoxElement<TE, TS, TP>
+    | PaperElement<TE, TS, TP>
+    | FieldsetElement<TE, TS, TP>
+    | AccordionElement<TE, TS, TP>
+    | ListElement<TE, TS, TP>
+    | TabsElement<TE, TS, TP>
+    | TabItemElement<TE, TS, TP>
+    | TableElement<TE, TS, TP>
+    | TableRowElement<TE, TS, TP>
+    | TableCellElement<TE, TS, TP>
+    | AlertElement<TE, TS, TP>
+    | CodeElement<TE, TS, TP>
+    | TextElement<TE, TS, TP>
+    | TitleElement<TE, TS, TP>
+    | QuoteElement<TE, TS, TP>
+    | FormattedNumberElement<TE, TS, TP>;
 export type MantineSize = "xs" | "sm" | "md" | "lg" | "xl";
 
 export type IconKeys = keyof Omit<
@@ -102,4 +122,92 @@ export interface TabItemElement<TE, TS, TP>
     extends MantineContainerElement<TE, TS, TP> {
     subtype: "tabItem";
     tabKey: string;
+}
+
+export interface TableElement<TE, TS, TP>
+    extends MantineContainerElement<TableRowElement<TE, TS, TP>, TS, TP> {
+    subtype: "table";
+    highlight?: DataType | boolean;
+    rowBorders?: DataType | boolean;
+    columnBorders?: DataType | boolean;
+    tableBorder?: DataType | boolean;
+    striped?: DataType | boolean;
+    caption?: DataType;
+}
+
+export interface TableRowElement<TE, TS, TP>
+    extends MantineContainerElement<TableCellElement<TE, TS, TP>, TS, TP> {
+    subtype: "tableRow";
+}
+
+export interface TableCellElement<TE, TS, TP>
+    extends MantineContainerElement<TE, TS, TP> {
+    subtype: "tableCell";
+    header?: boolean | DataType;
+}
+
+// Output Elements
+export interface MantineBaseElement<TE, TS, TP>
+    extends BaseElement<TE, TS, TP> {
+    pack: "mantine";
+}
+export interface AlertElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "alert";
+    color?: DataType;
+    variant?:
+        | DataType
+        | "default"
+        | "filled"
+        | "light"
+        | "outline"
+        | "transparent"
+        | "white";
+    icon?: IconKeys;
+    title?: DataType;
+}
+
+export interface CodeElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "code";
+    language?: DataType;
+    expandable?: DataType | boolean;
+    content?: DataType;
+}
+
+export interface TextElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "text";
+    bold?: DataType | number;
+    italic?: DataType | boolean;
+    content?: DataType;
+    size?: DataType | MantineSize | number;
+}
+
+export interface TitleElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "text";
+    bold?: DataType | number;
+    italic?: DataType | boolean;
+    content?: DataType;
+    order?: DataType | 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+export interface QuoteElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "quote";
+    icon?: IconKeys;
+    color?: DataType;
+    citation?: DataType;
+    content?: DataType;
+}
+
+export interface FormattedNumberElement<TE, TS, TP>
+    extends MantineBaseElement<TE, TS, TP> {
+    subtype: "formattedNumber";
+    prefix?: DataType;
+    suffix?: DataType;
+    thousandSeparator?: boolean | DataType;
+    decimalScale?: number | DataType;
+    value: DataType;
 }
